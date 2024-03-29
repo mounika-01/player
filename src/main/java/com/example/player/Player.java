2076133696fabd7bc8903625c167e2e42740791c
Mounika <bsmounika15@gmail.com>
2024-03-29 10:27:43 +0000

    solution

---
+++ src/main/java/com/example/player/Player.java
@@ -0,0 +1,69 @@
+// Write your code here.
+package com.example.player;
+
+public class Player {
+    private int playerId;
+    private String playerName;
+    private int jersyNumber;
+    private String role;
+
+    public Player(int playerId, String playerName, int jersyNumber,String role) {
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.jersyNumber = jersyNumber;
+        this.role = role;
+
+    public int getPlayerId() {
+        return playerId;
+    }
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+
+    public String getPlayerName() {
+        return playerName;
+
+        this.playerName = playerName;
+
+    public int getJersyNumber() {
+        return jersyNumber;
+    }
+     public void setJersyNumber(int  jersyNumber) {
+        this.jersyNumber = jersyNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+
+    
+    }
+
+}
+
+    
+    
+    
+
+    
+    
+
+    
+
+    
+
+     
+
+    
+
+    
+    
+
+    
+
+    
+
+    
+
+    
