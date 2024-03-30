86deb0c21d6386c1cb6d5cf16e02a37976fbc25a
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-03-30 15:53:12 +0530

    Update Player.java

--- src/main/java/com/example/player/Player.java
+++ src/main/java/com/example/player/Player.java
@@ -1,5 +1,5 @@
-// Write your code here.
 package com.example.player;
+
 
 public class Player {
     private int playerId;
@@ -7,63 +7,51 @@ public class Player {
     private int jerseyNumber;
     private String role;
 
-    public Player(int playerId, String playerName, int jerseyNumber,String role) {
+
+    public Player(int playerId, String playerName, int jerseyNumber, String role) {
         this.playerId = playerId;
         this.playerName = playerName;
         this.jerseyNumber = jerseyNumber;
         this.role = role;
+    }
+
 
     public int getPlayerId() {
         return playerId;
     }
+
+
     public void setPlayerId(int playerId) {
         this.playerId = playerId;
+    }
+
 
     public String getPlayerName() {
         return playerName;
+    }
 
+
+    public void setPlayerName(String playerName) {
         this.playerName = playerName;
+    }
+
 
     public int getJerseyNumber() {
         return jerseyNumber;
     }
-     public void setJerseyNumber(int  jerseyNumber) {
+
+
+    public void setJerseyNumber(int jerseyNumber) {
         this.jerseyNumber = jerseyNumber;
     }
+
 
     public String getRole() {
         return role;
     }
+
+
     public void setRole(String role) {
         this.role = role;
-
-    
     }
-
 }
-
-    
-    
-    
-
-    
-    
-
-    
-
-    
-
-     
-
-    
-
-    
-    
-
-    
-
-    
-
-    
-
-    
