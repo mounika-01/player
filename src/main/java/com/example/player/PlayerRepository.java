e176a18b5d61f6eea8760cb5fda1d8cf9a8dd3d1
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-06-25 13:01:25 +0530

    Update PlayerRepository.java

--- src/main/java/com/example/player/PlayerRepository.java
+++ src/main/java/com/example/player/PlayerRepository.java
@@ -1,15 +1,12 @@
-// Write your code here
 package com.example.player;
 
 import java.util.*;
-
 import com.example.player.Player;
 
 public interface PlayerRepository {
-
     ArrayList<Player> getPlayers();
 
-    Player getPlayerById(int playerId);
+    Player getPlayer(int playerId);
 
     Player addPlayer(Player player);
 
