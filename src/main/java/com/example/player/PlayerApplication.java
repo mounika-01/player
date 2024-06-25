97aad2a475efe3f10eaf033f9f29e14ed8d11490
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-06-25 13:00:22 +0530

    Update PlayerApplication.java

--- src/main/java/com/example/player/PlayerApplication.java
+++ src/main/java/com/example/player/PlayerApplication.java
@@ -1,5 +1,3 @@
-// Don't modify the code in this file
-
 package com.example.player;
 
 import org.springframework.boot.SpringApplication;
@@ -7,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlayerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PlayerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerApplication.class, args);
+    }
 }
